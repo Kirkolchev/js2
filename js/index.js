4a4e5611259056a4d5f81fe97503d303821bf665
kirKolchev <kirkol4ev@gmail.com>
2025-11-04 19:24:53 +0200

    asd

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-let result = (0.1 + 0.2).toFixed(1);
-console.log(result);
+// let result = (0.1 + 0.2).toFixed(1);
+// console.log(result);
 
 
-let a = "1";
-let b = 2;
-let sum = Number(a) + b;
-console.log(sum);
+// let a = "1";
+// let b = 2;
+// let sum = Number(a) + b;
+// console.log(sum);
 
 
 
-let flashGb = prompt("Введіть обсяг флешки у ГБ");
+// let flashGb = prompt("Введіть обсяг флешки у ГБ");
 
-flashGb = Number(flashGb);
-let flashMb = flashGb * 1024;
-let fileSize = 820;
-let fileCount = Math.floor(flashMb / fileSize);
-alert(`На флешку ${flashGb} ГБ поміститься ${fileCount} файлів по 820 МБ.`);
+// flashGb = Number(flashGb);
+// let flashMb = flashGb * 1024;
+// let fileSize = 820;
+// let fileCount = Math.floor(flashMb / fileSize);
+// alert(`На флешку ${flashGb} ГБ поміститься ${fileCount} файлів по 820 МБ.`);
