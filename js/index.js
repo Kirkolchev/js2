d2f240aa0e8a6ae064b26ed5089747639af4c56a
kirKolchev <kirkol4ev@gmail.com>
2025-11-04 19:19:29 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,18 +1,82 @@
-let result = (0.1 + 0.2).toFixed(1);
-console.log(result);
+let age = prompt('Який ваг вік?');
+let message = (age < 12) ? 'Вітаю, Дитино!' :
+    (age < 18) ? 'Вітаю  підліток' :
+        (age < 60) ? 'Моє шанування дорослий' :
+            (age < 100) ? 'Вітаю пенсіонер' :
+                (age >= 100) ? 'Вітаю довгожителю' :
+                    'некоректно введений вік';
+
+alert(message);
+
+let namber = prompt('Введіть будь-яке число від 0 до 9');
+let nam = (namber == 0) ? ')' :
+    (namber == 1) ? '!' :
+        (namber == 2) ? '@' :
+            (namber == 3) ? '#' :
+                (namber == 4) ? '$' :
+                    (namber == 5) ? '%' :
+                        (namber == 6) ? '^' :
+                            (namber == 7) ? '&' :
+                                (namber == 8) ? '*' :
+                                    (namber == 9) ? '(' :
+                                        'некоректно введене число';
+
+alert(nam);
 
 
-let a = "1";
-let b = 2;
-let sum = Number(a) + b;
-console.log(sum);
+
+let start = parseInt(prompt("Введіть початок діапазону:"));
+let end = parseInt(prompt("Введіть кінець діапазону:"));
+
+if (isNaN(start) || isNaN(end)) {
+    alert("Будь ласка, введіть коректні числа!");
+} else {
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    alert(`Сума чисел від ${start} до ${end} дорівнює ${sum}`);
+}
 
 
 
-let flashGb = prompt("Введіть обсяг флешки у ГБ");
 
-flashGb = Number(flashGb);
-let flashMb = flashGb * 1024;
-let fileSize = 820;
-let fileCount = Math.floor(flashMb / fileSize);
-alert(`На флешку ${flashGb} ГБ поміститься ${fileCount} файлів по 820 МБ.`);
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+let num1 = parseInt(prompt("Введіть перше число:"));
+let num2 = parseInt(prompt("Введіть друге число:"));
+
+if (isNaN(num1) || isNaN(num2)) {
+    alert("Будь ласка, введіть коректні числа!");
+} else {
+    let result = gcd(num1, num2);
+    alert(`Найбільший спільний дільник чисел ${num1} і ${num2} — ${result}`);
+}
+
+
+
+const number = parseInt(prompt("Введіть число:"));
+
+
+if (isNaN(number)) {
+    console.log("Будь ласка, введіть коректне число!");
+} else {
+    console.log(`Дільники числа ${number}:`);
+
+    // Шукаємо всі дільники
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            console.log(i);
+        }
+    }
+}
